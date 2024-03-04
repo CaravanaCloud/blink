b5986e72a5ac0a9562f51da4b53eccfd56c74a5e
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-03-04 13:15:00 +0000

    wip

--- blink-api/src/main/java/blink/api/domain/model/DonationCenter.java
+++ blink-api/src/main/java/blink/api/domain/model/DonationCenter.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
 import lombok.Data;
 
 @Entity
 @Data
+@NamedQuery(name = "DonationCenter.byLatLng", query = "SELECT d FROM DonationCenter d ORDER BY (d.longitude-:longitude)*(d.longitude-:longitude) + (d.latitude-:latitude)*(d.latitude-:latitude)")
 public class DonationCenter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- blink-api/src/main/java/blink/api/domain/repository/DonationCenterRepository.java
+++ blink-api/src/main/java/blink/api/domain/repository/DonationCenterRepository.java
@@ -1,6 +1,7 @@
 package blink.api.domain.repository;
 
 import blink.api.domain.model.DonationCenter;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -10,9 +11,9 @@ import java.util.List;
 @ApplicationScoped
 public class DonationCenterRepository implements PanacheRepository<DonationCenter> {
     public List<DonationCenter> findNearestDonationCenter(Double longitude, Double latitude) {
-        return find(
-                "SELECT d FROM DonationCenter d ORDER BY (d.longitude-:longitude)*(d.longitude-:longitude) + (d.latitude-:latitude)*(d.latitude-:latitude)",
-                        Parameters.with("longitude", longitude).and("latitude", latitude)).list();
-
+    PanacheQuery<DonationCenter> query = find("DonationCenter.byLatLng", 
+                                               Parameters.with("longitude", longitude)
+                                                         .and("latitude", latitude));
+    return query.list();   
     }
 }
